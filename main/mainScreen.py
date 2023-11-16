772c74357d7fae5a405844a5cb91d821247a506f
thanhdang0909 <dangnghialap2003@gmail.com>
2023-11-16 20:37:37 +0700

    Create mainScreen.py

---
+++ main/mainScreen.py
@@ -0,0 +1,34 @@
+import socket
+import pyautogui
+import firebase_admin
+from firebase_admin import credentials, storage
+from datetime import datetime
+
+# Khởi tạo Firebase
+cred = credentials.Certificate("path/to/your/firebase/credentials.json")
+firebase_admin.initialize_app(cred, {"storageBucket": "your-firebase-bucket-url"})
+
+# Kết nối đến server
+server_address = ('192.168.1.10', 8000)  
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(server_address)
+
+while True:
+    data = client_socket.recv(1024).decode('utf-8')
+
+    if data == "takeScreen":
+        # Chụp ảnh màn hình
+        screenshot = pyautogui.screenshot()
+        now = datetime.now()
+        nameScreen = "screenshot"+ now.strftime("%H%M%S") +".png"
+
+        # Lưu ảnh vào Firebase Storage
+        bucket = storage.bucket()
+        blob = bucket.blob(nameScreen)
+        blob.upload_from_file(nameScreen)
+
+        # Gửi link ảnh đến server
+        link = blob.public_url
+        client_socket.send(link.encode('utf-8'))
+
+client_socket.close()
