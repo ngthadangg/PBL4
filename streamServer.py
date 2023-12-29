b8bd81cb02f05263664f7ae24bdae5f3702218e8
thanhdang0909 <dangnghialap2003@gmail.com>
2023-12-29 23:29:49 +0700

    update stream

--- streamServer.py
+++ streamServer.py
@@ -1,70 +1,46 @@
-# server_and_flask.py
-import socket
 import cv2
-import numpy as np
-import zlib
-from flask import Flask, render_template, Response
-import threading
-import random
+import socket
+import pickle
 
-app = Flask(__name__)
+UDP_MAX_SIZE = 65500
+buffer = []
+# Tạo socket UDP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(("0.0.0.0", 9999))
 
-UDP_IP = "192.168.1.5"
-# UDP_PORT = 46364
-UDP_PORT = random.randint(1024, 49151)
-print("UDP_PORT:", UDP_PORT)
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+# Nhận yêu cầu kết nối từ client
+data, client_address = server_socket.recvfrom(1024)
+print("Connected to:", client_address)
 
-cap = cv2.VideoCapture(0)  # You can adjust the camera index if needed
+# Khởi tạo OpenCV để hiển thị video
+cv2.namedWindow("Server")
 
-def generate_frames():
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            # Convert the frame to JPEG format
-            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-            _, imgencode = cv2.imencode('.jpg', frame, encode_param)
-            data = np.array(imgencode)
+# Nhận dữ liệu từ client và hiển thị
+while True:
+    data, addr = server_socket.recvfrom(UDP_MAX_SIZE)
 
-            # Compress the data using zlib
-            compressed_data = zlib.compress(data, zlib.Z_BEST_COMPRESSION)
+    # Kiểm tra xem dữ liệu có đủ để unpickle không
+    if len(data) > 0:
+        try:
+            # Thêm dữ liệu vào bộ đệm
+            buffer.append(data)
 
-            # Yield the compressed data
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + compressed_data + b'\r\n')
+            # Nếu bộ đệm đầy, hiển thị dữ liệu
+            if len(buffer) >= UDP_MAX_SIZE:
+                frame = pickle.loads(b"".join(buffer))
+                cv2.imshow("Server", frame)
+                buffer = []
 
-def udp_server():
-    while True:
-        data, addr = sock.recvfrom(65507)  # 65507 is the maximum size of a UDP packet
-
-        # Decode the received data
-        img = cv2.imdecode(np.frombuffer(data, dtype=np.uint8), 1)
-
-        # Check if the image is valid
-        if img is not None and img.shape[0] > 0 and img.shape[1] > 0:
-            # Display the received video frame
-            cv2.imshow('Server Stream', img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            # Kiểm tra lỗi
+            if not len(data):
                 break
+        except pickle.UnpicklingError as e:
+            print(f"Unpickling error: {e}")
+    else:
+        print("Received empty data")
 
-    cv2.destroyAllWindows()
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
-
-@app.route('/')
-def index():
-    return render_template('stream.html')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    # Start the UDP server in a separate thread
-    udp_thread = threading.Thread(target=udp_server)
-    udp_thread.start()
-
-    # Start the Flask app
-    app.run(debug=True, port=8000)
+cv2.destroyAllWindows()
+server_socket.close()
