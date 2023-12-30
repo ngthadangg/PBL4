208a5dd4ef15aab2dcc18eda72a4e5aa1025fbd0
thanhdang0909 <dangnghialap2003@gmail.com>
2023-12-30 14:03:06 +0700

    updateWebCamServer

--- streamServer.py
+++ streamServer.py
@@ -1,46 +1,39 @@
 import cv2
 import socket
 import pickle
+import struct
 
-UDP_MAX_SIZE = 65500
-buffer = []
-# Tạo socket UDP
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("0.0.0.0", 9999))
+server_socket.listen(1)
 
-# Nhận yêu cầu kết nối từ client
-data, client_address = server_socket.recvfrom(1024)
+print("Waiting for a connection...")
+client_socket, client_address = server_socket.accept()
 print("Connected to:", client_address)
 
-# Khởi tạo OpenCV để hiển thị video
-cv2.namedWindow("Server")
+data = b""
+payload_size = struct.calcsize("L")
 
-# Nhận dữ liệu từ client và hiển thị
 while True:
-    data, addr = server_socket.recvfrom(UDP_MAX_SIZE)
+    while len(data) < payload_size:
+        data += client_socket.recv(4096)
 
-    # Kiểm tra xem dữ liệu có đủ để unpickle không
-    if len(data) > 0:
-        try:
-            # Thêm dữ liệu vào bộ đệm
-            buffer.append(data)
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("L", packed_msg_size)[0]
 
-            # Nếu bộ đệm đầy, hiển thị dữ liệu
-            if len(buffer) >= UDP_MAX_SIZE:
-                frame = pickle.loads(b"".join(buffer))
-                cv2.imshow("Server", frame)
-                buffer = []
+    while len(data) < msg_size:
+        data += client_socket.recv(4096)
 
-            # Kiểm tra lỗi
-            if not len(data):
-                break
-        except pickle.UnpicklingError as e:
-            print(f"Unpickling error: {e}")
-    else:
-        print("Received empty data")
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
+
+    cv2.imshow("Server", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+client_socket.close()
 cv2.destroyAllWindows()
 server_socket.close()
